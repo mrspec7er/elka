0ce2971099e158d769e40110f93dcd908a8cfa02
mrspec7er <wijayakusumasandi@gmail.com>
2023-08-07 08:46:42 +0800

    Init Project

---
+++ app/components/footer.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Footer } from "flowbite-react";
+
+export default function MainFooter() {
+  return (
+    <Footer className="bg-gray-700 rounded-none" container>
+      <Footer.Copyright
+        className="text-white"
+        by="Elka™"
+        href="/"
+        year={2023}
+      />
+      <Footer.LinkGroup className="text-white">
+        <Footer.Link href="/">About</Footer.Link>
+        <Footer.Link href="/">Privacy Policy</Footer.Link>
+        <Footer.Link href="/">Licensing</Footer.Link>
+        <Footer.Link href="/">Contact</Footer.Link>
+      </Footer.LinkGroup>
+    </Footer>
+  );
+}
---
+++ app/components/navbar.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Navbar } from "flowbite-react";
+
+export default function MainNavbar() {
+  return (
+    <Navbar className="bg-gray-700" fluid rounded>
+      <Navbar.Brand href="https://flowbite-react.com">
+        <img
+          alt="Flowbite React Logo"
+          className="mr-3 h-6 sm:h-9"
+          src="https://www.flowbite-react.com/favicon.svg"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Elka Code
+        </span>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Navbar.Link active className="text-white" href="/">
+          <p>Home</p>
+        </Navbar.Link>
+        <Navbar.Link className="text-white">
+          <p>About</p>
+        </Navbar.Link>
+        <Navbar.Link href="/" className="text-white">
+          Services
+        </Navbar.Link>
+        <Navbar.Link href="/" className="text-white">
+          Pricing
+        </Navbar.Link>
+        <Navbar.Link href="/" className="text-white">
+          Contact
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
