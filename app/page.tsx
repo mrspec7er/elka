3ccc2fdf2c968e65f5b651ac5183034c7c2130a7
mrspec7er <wijayakusumasandi@gmail.com>
2023-08-08 09:19:37 +0800

    feat: 🎸 landing
    added second section ui

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <MainNavbar />
       <main>
         <FirstSection />
+        <SecondSection />
       </main>
       <MainFooter />
     </>
@@ -28,10 +29,52 @@ function FirstSection() {
         </div>
         <div>
           <img
-            className="h-auto absolute -bottom-40 right-0"
-            src="/eiffel.png"
-            alt="company-icon"
+            className="h-[100%] absolute -bottom-[15%] right-0"
+            src="/content1.png"
+            alt="company"
           />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function SecondSection() {
+  return (
+    <div className="relative">
+      <div className="container mx-auto pl-3 flex flex-col-reverse md:flex-row items-center md:pl-[30vw] py-32 min-h-[85vh]">
+        <div>
+          <img
+            className="absolute left-0 2xl:w-[10vw] lg:w-[20vw] md:w-[30vw] w-[50vw] md:top-[30%] top-[15%]"
+            src="/content2.png"
+            alt="presentation"
+          />
+        </div>
+        <div className="z-10 max-sm:pt-[42vh]">
+          <p className="text-3xl mb-2">Company Tagline</p>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+            voluptas similique, maxime, corrupti possimus fuga obcaecati
+            architecto suscipit accusamus iusto alias explicabo, id voluptate
+            nemo expedita dolores laudantium inventore minima! Alias adipisci
+            molestiae optio. Doloremque voluptatem quam excepturi nisi,
+            exercitationem earum quas? Perferendis ab velit dolorum illum
+            quibusdam quam exercitationem!
+          </p>
+          <p className="text-3xl mt-5 mb-2">Our Clients</p>
+          <div className="overflow-x-auto">
+            <div className="flex">
+              <img className="w-72 h-20" src="/logo.png" alt="clients" />
+              <img className="w-72 h-20" src="/logo.png" alt="clients" />
+              <img className="w-72 h-20" src="/logo.png" alt="clients" />
+              <img className="w-72 h-20" src="/logo.png" alt="clients" />
+              <img className="w-72 h-20" src="/logo.png" alt="clients" />
+              <img className="w-72 h-20" src="/logo.png" alt="clients" />
+              <img className="w-72 h-20" src="/logo.png" alt="clients" />
+              <img className="w-72 h-20" src="/logo.png" alt="clients" />
+              <img className="w-72 h-20" src="/logo.png" alt="clients" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
